84edf2bc03537344fcf0ba845191fa498257d5b9
Sahil Personal <sahilkharb7030@gmail.com>
2026-01-07 15:27:21 +0530

    New file added test2.py

---
+++ test2.py
@@ -0,0 +1,10 @@
+print('\n')
+print("stash files changes(changes are saved temporary but these aren't commited)")
+
+# commit
+# in order to commit something first it need to added in staged directory which requires git add command
+# git commit permanently add changes in github repo where git add allows you add changes in staged directory 
+# git add also allows control of which files need to added and it requires no message
+# git commit requires message attached with it
+
+# first do git add and then do git commit(helps track of changes in better way)
