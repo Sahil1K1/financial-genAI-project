d65e29703c60959c6b1b7f7f117c76f678dc5c55
Sahil Personal <sahilkharb7030@gmail.com>
2026-01-08 11:31:53 +0530

    text related to git commit --amend

--- test2.py
+++ test2.py
@@ -23,3 +23,21 @@ print("stash files changes(changes are saved temporary but these aren't commited
 
 # now once the changes are in local(master branch) now we can push these changes to github
 # git push origin master
+
+# now once the changes are merged to master, next is to create a tag
+# a tag is like a version of project(v1.0.0) and it is used for deployment in later phases.
+
+# in order to create a tag first we need to push all changes of personal branch into github
+# then checkout to master branch > take latest pull > merge personal into master(resolve conflict if any)
+# push changes to master
+
+# now stay on master and create a tag
+# simple and easy tag == git tag
+# if we want to add annotations with it like date, email and other information == git tag -a v1.0.0 -m "First stable version"
+# once tag is created then push those changes to master
+# git push origin v1.0.0
+
+# if user wanted to make some changes in old commits
+# first is to make changes in the latest commit then make changes and use git add .(to stage those changes)
+# then use git commit --amend (it will open editor to make changes in message)
+# if we don't want to change message as well then we can use == git commit --amend --no-edit
